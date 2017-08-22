6620b33963ed98b02e842952ad6c2baa2f60abf0
Anaid Labat <A01746289@itesm.mx>
2017-08-22 09:08:49 -0500

    Empece el ejercicio 1

--- miInfo.py
+++ miInfo.py
@@ -1,6 +1,6 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
+# Autor:Anaid Fernanda Labat González, A01746289
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # A partir de aquí escribe tu programa
